2612c42b37b3ac6b68ec8c8fd9f993d9f1e5f476
Luis <30819865+hlcxpl@users.noreply.github.com>
2022-05-24 01:27:52 -0400

    update

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # hlcxpl.github.io
+
--- assets/js/control.js
+++
@@ -1,7 +0,0 @@
-$(window).scroll(function(){
-    if($("#nav").offset().top>500){
-        $("#nav").addClass("navbar-black");
-    }else{
-        $("#nav").addClass("navbar-white");
-    }  
-});
